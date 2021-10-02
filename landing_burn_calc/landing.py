a7a9687a735f2def60c165301785ded1c9fef90d
Stephen Catsamas <seca8193@gmail.com>
2021-10-02 23:25:54 +1000

    working core simulator

--- landing_burn_calc/landing.py
+++ landing_burn_calc/landing.py
@@ -7,10 +7,30 @@ print("====LANDING BURN CALULATOR====")
 print("rev: 0.0.1")
 print("==============================")
 
-rs = 60E3;#surface height
-G = 6.67E-11;
-M = 2.6457E19;
+#Minmus
+# rs = 60E3;#surface height
+# G = 6.67E-11;
+# M = 2.6457E19;
+# T = 40400;
 
+#Mun
+rsea = 200E3;#surface height
+rs = 4E3 + rsea;#surface height
+G = 6.67E-11;
+M = 9.76E20;
+T = 138984;
+
+
+vfs = 2*pi/T;
+
+def get_TWR(thrust, r = rs):
+    m = 1
+    TWR = thrust / (G*M*m/(r*r))
+    return TWR
+def get_thrust(TWR, r = rs):
+    m = 1
+    thrust = TWR * (G*M*m/(r*r))
+    return thrust
 
 #Function: landing_system
 #landing burn implementatation of the system function for rK4
@@ -19,22 +39,20 @@ M = 2.6457E19;
 #  vec - initial velocity 
 #Returns:
 #    array of dxdt,dydt, the time derivatives of velocity evaluated at (vec,t)
-def landing_config(TWRc):
+def landing_config(thrust):
     def landing_system(t,vec):   
         #unpack location vector
         r,f,vr,vf=vec
         
         m = 1;
-        
         #trust
-        t = TWRc*G*M*m/(rs*rs);
-        t = 0
+        t = thrust#TWRc*G*M*m/(rs*rs);
         #decompose to retrograde burn
-        v = sqrt(vr*vr + r*vf*r*vf)
+        v = sqrt(vr*vr + (r*vf - rs* vfs)*(r*vf - rs* vfs))##accounting for surface rotation rs*vfs
 
         if(v > 1):
             tr = -vr/v * t
-            tf = -r*vf/v * t
+            tf = -(r*vf - rs* vfs)/v * t
         else:
             tr = t
             tf = 0
@@ -59,7 +77,7 @@ def intersect(A,B,C,D):
     return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
 #define initial contitions
-z0 = np.array([[63E3,0,0,235/63E3,0]])#x,y,t
+z0 = np.array([[11E3+rs,0,0,550/(1E3+rs),0]])#x,y,t
 #define initial step size
 h  = 1
 
@@ -69,12 +87,112 @@ err = np.array([[100*err,100*err,err,err]])
 
 solution = False
 
-##########
-system = landing_config(0)
-allloc = solver.stepper(z0, 800000, system, h, h_adj = True, error = err)
-############
-print(allloc)
+thrust = 5
+dthrust = 0.0001
 
+thrusthigh = 1
+thrustlow = 1
+thrusthighset = False
+thrustlowset = False
+
+while(not solution):
+    # solution = True
+    allloc = z0
+    landed = False;
+    
+    TWR = get_TWR(thrust)
+    print(TWR)
+    if(TWR <= 1):
+        print("error system will not converge")
+        exit()
+    system = landing_config(thrust)
+
+    while( not landed):
+        #solve the test ODE
+        stloc = np.array([allloc[-1]])
+        loc = solver.stepper(stloc, 10, system, h, h_adj = True, error = err)
+        allloc = np.vstack((allloc,loc))
+
+        r,f,vr,vf,t= allloc[-1]
+        
+        if((vr > 0) and (abs(vr) >= abs(r*vf - rs*vfs))):
+            landed = True
+    
+    end = [rs+0,rs+100,-3,3]
+
+    for i,vec in enumerate(loc):
+        r,f,vr,vf,t= vec
+        hit = False
+        #case that one of the points lies in the box
+        if ((end[0] <= r <= end[1]) and (end[2] <= vr <= end[3])):
+            hit = True
+            break;
+            # print('e')
+        else:
+        #case that the line segment crosses the box
+            if(i != 0):
+                r1 = r;
+                vr1 = vr;
+                r2 = loc[i-1][0]
+                vr2 = loc[i-1][2]
+               
+                if(intersect([r1,vr1],[r2,vr2],[end[1],end[2]],[end[1],end[3]])):
+                    hit = True
+                    break;
+                    # print('a')
+                if(intersect([r1,vr1],[r2,vr2],[end[1],end[3]],[end[0],end[3]])):
+                    hit = True
+                    break;
+                    # print('b')
+                if(intersect([r1,vr1],[r2,vr2],[end[0],end[3]],[end[0],end[2]])):
+                    hit = True
+                    break;
+                    # print('c')
+                if(intersect([r1,vr1],[r2,vr2],[end[0],end[2]],[end[1],end[2]])):
+                    hit = True
+                    break;
+                    # print('d')
+
+    solution = hit
+    vrmin = None
+    for vec in allloc:
+        r,f,vr,vf,t = vec
+        
+        if(abs(vr) >= abs(r*vf - rs*vfs)):
+            if(vrmin == None):
+                vrmin = abs(vr)
+            if(abs(vr) <= vrmin):
+                vrmin = abs(vr)
+                deltH = r-rs
+                
+    if(not(thrusthighset and thrustlowset)):
+        if(deltH > 0):
+             thrusthighset = True
+             thrusthigh = thrust
+        else:
+             thrustlowset = True
+             thrustlow = thrust
+        TWR = get_TWR(thrust)
+        if(not thrusthighset):
+            TWR = 1 + 2*(TWR-1)
+        elif(not thrustlowset):
+            TWR = 1 + 0.5*(TWR-1)
+        thrust = get_thrust(TWR)
+    if(thrusthighset and thrustlowset):
+        if(deltH > 0):
+            if(thrust > thrusthigh):
+                print('thrust not bounded')
+            thrusthigh = thrust
+        else:
+            if(thrust < thrustlow):
+                print('thrust not bounded')
+            thrustlow = thrust
+        thrust = (thrusthigh + thrustlow)/2
+
+    TWR = get_TWR(thrust)
+    if(TWR < 1.005):
+        thrust = get_thrust(1.005);
+    
 fig = plt.figure(figsize=(10,4)) 
 ax = fig.add_subplot(1, 2, 1) 
 ax2 = fig.add_subplot(1, 2, 2) 
@@ -86,119 +204,7 @@ ax2.set_xlabel('Radius $r$ (m)');
 ax2.set_ylabel('Radial Velocity $v_r$ (m/s)');
 # ax.axhline(0, color='r')
 # ax2.axvline(0, color='r')
-plt.show()
-
-
-
-
-# TWR = 2
-# dTWR = 0.0001
-
-# TWRhigh = 1
-# TWRlow = 1
-# TWRhighset = False
-# TWRlowset = False
-
-# while(not solution):
-    # solution = True
-    # allloc = z0
-    # landed = False;
-    
-    # print(TWR)
-    # if(TWR <= 1):
-        # print("error system will not converge")
-        # exit()
-    # system = landing_config(TWR)
-
-    # while( not landed):
-        # #solve the test ODE
-        # stloc = np.array([allloc[-1]])
-        # loc = solver.stepper(stloc, 10, system, h, h_adj = True, error = err)
-        # allloc = np.vstack((allloc,loc))
-
-        # x,z,vx,vz,t= allloc[-1]
-        
-        # if(vz > 0):
-            # landed = True
-    
-    # end = [0,100,-3,3]
-
-    # for i,vec in enumerate(loc):
-        # x,z,vx,vz,t= vec
-        # hit = False
-        # #case that one of the points lies in the box
-        # if ((end[0] <= z <= end[1]) and (end[2] <= vz <= end[3])):
-            # hit = True
-            # break;
-            # # print('e')
-        # else:
-        # #case that the line segment crosses the box
-            # if(i != 0):
-                # z1 = z;
-                # vz1 = vz;
-                # z2 = loc[i-1][1]
-                # vz2 = loc[i-1][3]
-               
-                # if(intersect([z1,vz1],[z2,vz2],[end[1],end[2]],[end[1],end[3]])):
-                    # hit = True
-                    # break;
-                    # # print('a')
-                # if(intersect([z1,vz1],[z2,vz2],[end[1],end[3]],[end[0],end[3]])):
-                    # hit = True
-                    # break;
-                    # # print('b')
-                # if(intersect([z1,vz1],[z2,vz2],[end[0],end[3]],[end[0],end[2]])):
-                    # hit = True
-                    # break;
-                    # # print('c')
-                # if(intersect([z1,vz1],[z2,vz2],[end[0],end[2]],[end[1],end[2]])):
-                    # hit = True
-                    # break;
-                    # # print('d')
-
-    
-
-    # solution = hit
-    
-    # vzmin = None
-    # for vec in allloc:
-        # x,z,vx,vz,t = vec
-        
-        # if(abs(vz) >= abs(vx)):
-            # if(vzmin == None):
-                # vzmin = abs(vz)
-            # if(abs(vz) < vzmin):
-                # vzmin = abs(vz)
-                # deltZ = z
-                
-    # if(not(TWRhighset and TWRlowset)):
-        # if(deltZ > 0):
-             # TWRhighset = True
-             # TWRhigh = TWR
-        # else:
-             # TWRlowset = True
-             # TWRlow = TWR
-    
-        # if(not TWRhighset):
-            # TWR = 1 + 2*(TWR-1)
-        # elif(not TWRlowset):
-            # TWR = 1 + 0.5*(TWR-1)
-    # if(TWRhighset and TWRlowset):
-        # if(deltZ > 0):
-            # if(TWR > TWRhigh):
-                # print('TWR not bounded')
-            # TWRhigh = TWR
-        # else:
-            # if(TWR < TWRlow):
-                # print('TWR not bounded')
-            # TWRlow = TWR
-        # TWR = (TWRhigh + TWRlow)/2
-
-    
-    # if(TWR < 1.005):
-        # TWR = 1.005
-    
-    
+plt.show() 
 
 
 
